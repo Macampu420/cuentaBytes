1fdf909f64123551218145796cd0b46f6d300ec3
Miguel Campuzano <campuzanomiguel2208@gmail.com>
2023-02-22 17:35:23 -0500

    se registra la imagen del producto

--- src/models/productos.php
+++ src/models/productos.php
@@ -1,48 +1,87 @@
-<?php 
+<?php
 
-    function registrar($sqlRegProduc, $conn){
+function registrarImg() {
+    if (isset($_FILES['imgProducto'])) {
+        $host = 'localhost';
+        $user = 'root';
+        $pass = '';
+        $db = 'cuentabytes';
 
-        $registro = $conn -> query("$sqlRegProduc");
+        $connection = new mysqli($host, $user, $pass, $db);
+        $nombreArchivo = $_FILES['imgProducto']['name'];
+        $tipo_archivo = $_FILES['imgProducto']['type'];
+        $ruta_temporal = $_FILES['imgProducto']['tmp_name'];
 
-        if(!$registro){
-            echo "error en el registro";
-        } else {
-            echo "Registro exitoso";
+        // Obtener la extensión del archivo
+        $extension = pathinfo($nombreArchivo, PATHINFO_EXTENSION);
+
+        // Generar un nuevo nombre para la imagen
+        $nombreimg = date('YmdHis') . "." . $extension;
+
+        // Verificamos que el archivo sea una imagen
+        if ($tipo_archivo == "image/jpeg" || $tipo_archivo == "image/png") {
+            $ruta_destino = "./../../../public/img/productos/";
+
+            // Movemos el archivo de su ubicación temporal a la carpeta destino
+            move_uploaded_file($ruta_temporal, $ruta_destino.$nombreimg);
+            $resultado = $connection -> query('call registrarImagen("'.$ruta_destino.'", "'.$nombreimg.'")');
+
+            if ($resultado === TRUE) {
+                // Obtener el ID del registro insertado
+                $id_insertado = $connection->insert_id;
+                return $id_insertado;
+            } else {
+                echo "Error al insertar el registro: " . $connection->error;
+            }
         }
 
-        header("Location: http://localhost/cuentabytes/src/views/productos.php");
-        
+        $connection -> close();
+    }
+}
+
+function registrar($sqlRegProduc, $conn) {
+
+    $registro = $conn -> query("$sqlRegProduc");
+
+    if (!$registro) {
+        echo "error en el registro";
+    } else {
+        echo "Registro exitoso";
     }
 
-    function eliminarProducto($sqlEliminar, $conn){
+    header("Location: http://localhost/cuentabytes/src/views/productos.php");
 
-        $resEliminar = $conn -> query($sqlEliminar);
-    
-        if(!$resEliminar){
-            echo "ha ocurrido un error al eliminar el producto";
-        } else {
-            echo "Producto eliminado correctamente";
-        }
+}
+
+function eliminarProducto($sqlEliminar, $conn) {
+
+    $resEliminar = $conn -> query($sqlEliminar);
+
+    if (!$resEliminar) {
+        echo "ha ocurrido un error al eliminar el producto";
+    } else {
+        echo "Producto eliminado correctamente";
     }
-    
-    function listarUnProducto($sqlListar, $conn){
-    
-        $producto = $conn->query($sqlListar);
-    
-        return $producto;
-    
+}
+
+function listarUnProducto($sqlListar, $conn) {
+
+    $producto = $conn -> query($sqlListar);
+
+    return $producto;
+
+}
+
+function actualizarProducto($sqlActualizar, $conn) {
+
+    $resActualizar = $conn -> query($sqlActualizar);
+
+    if (!$resActualizar) {
+        echo "ha ocurrido un error al actualizar el producto";
+    } else {
+        echo "Producto actualizado correctamente";
     }
-    
-    function actualizarProducto($sqlActualizar, $conn){
-    
-        $resActualizar = $conn->query($sqlActualizar);
-    
-        if(!$resActualizar){
-            echo "ha ocurrido un error al actualizar el producto";
-        } else {
-            echo "Producto actualizado correctamente";
-        }
-    
-        header("Location: http://localhost/cuentabytes/src/views/productos.php");
-    }
+
+    header("Location: http://localhost/cuentabytes/src/views/productos.php");
+} 
 ?>
