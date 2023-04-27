797d320008d1ef538a2f528d721aded8f7418cce
Miguel Campuzano <campuzanomiguel2208@gmail.com>
2023-04-27 12:30:39 -0500

    ctr ajustes comentado

--- public/js/ajustes/ctrAjustes.js
+++ public/js/ajustes/ctrAjustes.js
@@ -1,15 +1,17 @@
 let btnDesplegarAjustes = document.getElementById('btnDesplegarAjustes');
 var offcanvasAjustes = new bootstrap.Offcanvas(document.getElementById('offcanvasAjustes'));
-
+let ajustes;
 btnDesplegarAjustes.onclick = () => {
     offcanvasAjustes.show();
 }
 
 let traerAjustes = async () => {
+    //se consume el API de los ajustes
     let resAjustes = await fetch('http://localhost:3000/ajustes');
-    let ajustes = await resAjustes.json();
+    ajustes = await resAjustes.json();
 
+    //se pone el nombre de la empresa en el header de la vista
     document.getElementById("pHeader").innerHTML = ajustes.nombreEmpresa;
 }
 
-document.addEventListener("DOMContentLoaded", () => {traerAjustes()});
+document.addEventListener("DOMContentLoaded", () => traerAjustes());
--- src/models/ajustes.js
+++ src/models/ajustes.js
@@ -5,11 +5,13 @@ class Ajustes {
     servirAjustes = async (req, res) => {
         try {
 
+            //se traen los datos de la bd y si todo sale bien se sirve la API
             let resAjutes = await pool.query("CALL listarAjustes");
             let ajustes = JSON.stringify(resAjutes[0][0]);
             res.status(200).send(ajustes);
 
         } catch (error) {
+            //se imprime el error y se envia un json al cliente
             console.log(error);
             res.status(500).send(JSON.stringify({
                 error: "Ha ocurrido un error al consultar los ajustes"
